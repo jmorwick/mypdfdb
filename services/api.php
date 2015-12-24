7f90cd4e0113c6803cd6ef3740e5d23358e95a49
jmorwick <jbmorwick@gmail.com>
2015-12-24 10:08:52 -0600

    moved error messages to functions

--- services/api.php
+++ services/api.php
@@ -1,5 +1,32 @@
 <?php
 
+////////////////////////////////////////////////////////////////////////////////
+// error messages
+////////////////////////////////////////////////////////////////////////////////
+
+function err_no_such_service($service) {
+	echo "404: no such service: $service";	
+	http_response_code(404);
+	die();
+}
+
+function err_bad_input_format($msg) {
+	echo "404: malformed input to service: $msg";	
+	http_response_code(404);
+	die();
+}
+
+function err_bad_input_data($field, $value, $msg) {
+	echo "404: bad input value for service: '$field' can't have value '$value': $msg";	
+	http_response_code(403);
+	die();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// begin service handling script
+////////////////////////////////////////////////////////////////////////////////
+
 header("Access-Control-Allow-Orgin: *");
 header("Access-Control-Allow-Methods: *");
 
@@ -22,33 +49,28 @@ switch($method) {
 				search_db($args, $_GET);
 				die();
 			case 'pdf':
-				retreive_pdf($args);
-			default:
-				http_response_code(404);
-				die();
+				retrieve_pdf($args);
+			default: err_no_such_service($endpoint);
 		}
 	case 'POST':
 		switch($endpoint) {
-			default:
-				http_response_code(404);
-				die();
+			default: err_no_such_service($endpoint);
 		}
 	case 'PUT':
 		switch($endpoint) {
-			default:
-				http_response_code(404);
-				die();
+			default: err_no_such_service($endpoint);
 		}
 	case 'DELETE':
 		switch($endpoint) {
-			default:
-				http_response_code(404);
-				die();
+			default: err_no_such_service($endpoint);
 		}
-	default:
-		http_response_code(404);
-		die();
+	default: err_no_such_service($endpoint);
 }
+
+
+////////////////////////////////////////////////////////////////////////////////
+// helper functions
+////////////////////////////////////////////////////////////////////////////////
 
 function is_tag($str) {
 	return false;  // TODO: check for existence of tag in DB
@@ -61,36 +83,38 @@ function is_pdf_id($str) {
 function retrieve_pdf() {
 	global $data_dir, $args;
 	
-	if(count($args) != 1 || !is_pdf_id($args[0])) {
-		http_response_code(400);
-		die();
-	}
+	if(count($args) != 1)
+		err_bad_input_format("expected one argument in URL");
+	
+	if(!is_pdf_id($args[0]))
+		err_bad_input_data('file_id', $args[0], 'not a valid pdf id');
 	
 	$filename = get_pdf_details($args[0])['filename'];
-	if(!$filename) {
-		http_response_code(404);
-		die();
-	}
+	if(!$filename) err_bad_input_data('file_id', $args[0], "pdf file missing from database");
 	
 	header('Content-Type: application/pdf');
 	readfile($data_dir, $filename);
 }
 
+function get_pdf_details($id) {
+	return array(); // TODO: get tuple from files table
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// servicing functions
+////////////////////////////////////////////////////////////////////////////////
+
 function search_db() {
 	global $args;
 	
-	if(count($args) > 1) {
-		http_response_code(400);
-		die();
-	}
+	if(count($args) > 1) 
+		err_bad_input_format("exactly one url argument expected");
 	
 	// validate tag argument
 	if(count($args) == 1 || isset($extra_args['tag'])) { // validate tag
 		$tag = isset($args['tag']) ? $extra_args['tag'] : $args[0];
-		if(!is_tag($tag)) {
-			http_response_code(403);
-			die();
-		}
+		if(!is_tag($tag)) 
+			err_bad_input_data('tag', $tag, "not a valid tag");
 	}
 	
 	
