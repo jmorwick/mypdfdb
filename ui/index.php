880062f833b4d19a24109a7dccb1d1ad7d3d3b04
jmorwick <jbmorwick@gmail.com>
2015-12-26 23:18:56 -0600

    added in complete github repos for treeview and table plugins

--- ui/index.php
+++ ui/index.php
@@ -4,11 +4,13 @@
 <meta charset="UTF-8"/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-<script src="jquery.dynatable.js"></script>
+<script src="jquery-dynatable/jquery.dynatable.js"></script>
+<script src="jquery-bonsai/jquery.bonsai.js"></script>
 <script src="mypdfdb.js"></script>
 <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css"></link>
 <link rel="stylesheet" href="mypdfdb.css"></link
-<link rel="stylesheet" href="jquery.dynatable.css"></link>
+<link rel="stylesheet" href="jquery-dynatable/jquery.dynatable.css"></link>
+<link rel="stylesheet" href="jquery-bonsai/jquery.bonsai.css"></link>
 <title>MyPDF DB</title>
 </head>
 
